169cf00310af4b72fd6eaaa472db91d026b38724
TheAlmightyMight <klimentykk@gmail.com>
2022-05-30 12:21:18 +0300

    Logic finished

--- calculator/src/components/keys.js
+++ calculator/src/components/keys.js
@@ -1,26 +1,159 @@
 import React from "react";
+import { ACTIONS } from "../App";
 
-function Keys() {
+function Keys({ dispatch }) {
   return (
     <div className="btn-container">
-      <div className="Btn">7</div>
-      <div className="Btn">8</div>
-      <div className="Btn">9</div>
-      <div className="Btn btn-delete">DEL</div>
-      <div className="Btn">4</div>
-      <div className="Btn">5</div>
-      <div className="Btn">6</div>
-      <div className="Btn">+</div>
-      <div className="Btn">1</div>
-      <div className="Btn">2</div>
-      <div className="Btn">3</div>
-      <div className="Btn">-</div>
-      <div className="Btn">.</div>
-      <div className="Btn">0</div>
-      <div className="Btn">/</div>
-      <div className="Btn">x</div>
-      <div className="Btn btn-reset">RESET</div>
-      <div className="Btn btn-equality">=</div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 7 } })
+        }
+        className="Btn"
+      >
+        7
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 8 } })
+        }
+        className="Btn"
+      >
+        8
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 9 } })
+        }
+        className="Btn"
+      >
+        9
+      </div>
+      <div
+        onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}
+        className="Btn btn-delete"
+      >
+        DEL
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 4 } })
+        }
+        className="Btn"
+      >
+        4
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 5 } })
+        }
+        className="Btn"
+      >
+        5
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 6 } })
+        }
+        className="Btn"
+      >
+        6
+      </div>
+      <div
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.CHOOSE_OPERATION,
+            payload: { operation: "+" },
+          })
+        }
+        className="Btn"
+      >
+        +
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } })
+        }
+        className="Btn"
+      >
+        1
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 2 } })
+        }
+        className="Btn"
+      >
+        2
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 3 } })
+        }
+        className="Btn"
+      >
+        3
+      </div>
+      <div
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.CHOOSE_OPERATION,
+            payload: { operation: "-" },
+          })
+        }
+        className="Btn"
+      >
+        -
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: "." } })
+        }
+        className="Btn"
+      >
+        .
+      </div>
+      <div
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 0 } })
+        }
+        className="Btn"
+      >
+        0
+      </div>
+      <div
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.CHOOSE_OPERATION,
+            payload: { operation: "÷" },
+          })
+        }
+        className="Btn"
+      >
+        /
+      </div>
+      <div
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.CHOOSE_OPERATION,
+            payload: { operation: "*" },
+          })
+        }
+        className="Btn"
+      >
+        x
+      </div>
+      <div
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+        className="Btn btn-reset"
+      >
+        RESET
+      </div>
+      <div
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+        className="Btn btn-equality"
+      >
+        =
+      </div>
     </div>
   );
 }
--- calculator/src/components/output.js
+++ calculator/src/components/output.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-function Input() {
+function Output({ data, formatOperand }) {
+  const { currentOperand, previousOperand, operation, result } = data;
+  console.log(currentOperand);
   return (
     <div className="input-container">
-      {/* <div className="previous-operand">
-        {formatOperand(previousOperand)} {operation}
+      <div className="previous-operand">
+        {formatOperand(previousOperand)}
+        {operation}
       </div>
-      <div className="current-operand">{formatOperand(currentOperand)}</div> */}
+      <div className="current-operand">{formatOperand(currentOperand)}</div>
     </div>
   );
 }
 
-export default Input;
+export default Output;
